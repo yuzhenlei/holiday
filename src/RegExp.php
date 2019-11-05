79109642d9a5124b8a825e01de57503b072d9492
yuzhenlei <yuzhenlei01@gmail.com>
2019-11-05 16:36:54 +0800

    fix bug

--- src/RegExp.php
+++ src/RegExp.php
@@ -15,27 +15,27 @@ class RegExp
 
     public static function holidayRangeT1()
     {
-        return '/(\d{4}+)年(\d{1,2}+)月(\d{1,2}+)日[至—](\d{4}+)年(\d{1,2}+)月(\d{1,2}+)日[放假|调休]+/';
+        return '/(\d{4}+)年(\d{1,2}+)月(\d{1,2}+)日[至—]+(\d{4}+)年(\d{1,2}+)月(\d{1,2}+)日[放假|调休]+/';
     }
 
     public static function holidayRangeT2()
     {
-        return '/(\d{1,2}+)月(\d{1,2}+)日[至—](\d{1,2}+)月(\d{1,2}+)日[放假|调休]+/';
+        return '/[^年](\d{1,2}+)月(\d{1,2}+)日[至—]+[^年](\d{1,2}+)月(\d{1,2}+)日[放假|调休]+/';
     }
 
     public static function holidayRangeT3()
     {
-        return '/(\d{1,2}+)月(\d{1,2}+)日[至—](\d{1,2}+)日[放假|调休]+/';
+        return '/[^年](\d{1,2}+)月(\d{1,2}+)日[至—]+(\d{1,2}+)日[放假|调休]+/';
     }
 
     public static function holidayRangeT4()
     {
-        return '/(\d{1,2}+)月(\d{1,2}+)日[放假|调休]+/';
+        return '/[^年](\d{1,2}+)月(\d{1,2}+)日[放假|调休]+/';
     }
 
     public static function holidayRangeT5()
     {
-        return '/(\d{1,2}+)月(\d{1,2}+)日放假，(\d{1,2}+)月(\d{1,2}+)日.*?补休/';
+        return '/[^年](\d{1,2}+)月(\d{1,2}+)日放假，[^年](\d{1,2}+)月(\d{1,2}+)日.*?补休/';
     }
 
     public static function adjustDateT1()
